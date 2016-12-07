14dac890dd6a5d0a0ea9012b012e69b0bd60daeb
John Kelvie <john.kelvie@gmail.com>
2016-12-07 13:36:52 -0500

    Added resume intent when in resume mode Added auto RSS refresh (every second) Added code coverage

--- test/audioManagerTest.js
+++ test/audioManagerTest.js
@@ -5,7 +5,7 @@ describe('AudioManager', function() {
     describe('#fromURL', function() {
         it("Correctly loads RSS feed from file", function(done) {
             this.timeout(10000);
-            AudioManager.load('file', 'test/rssFeed.xml', function () {
+            AudioManager.load('file', 'test/rssFeed.xml', 60, function () {
                 assert.equal(AudioManager.feed().items.length, 6);
                 assert(AudioManager.feed());
                 AudioManager.introductorySSML(function (ssml) {
@@ -15,10 +15,27 @@ describe('AudioManager', function() {
             });
         });
 
+        it("Correctly reloads RSS feed from URL", function(done) {
+            this.timeout(10000);
+            AudioManager.reset();
+            AudioManager.load('URL', 'http://bespoken.libsyn.com/rss', 1, function () {
+                assert.equal(AudioManager.feed().items.length, 3);
+                assert(AudioManager.feed());
+                setTimeout(function () {
+                    AudioManager.load('URL', 'http://bespoken.libsyn.com/rss', 1, function (error, feed, refreshed) {
+                        assert(refreshed);
+                        assert(feed);
+                        done();
+                    });
+                }, 2000);
+
+            });
+        });
+
         it("Correctly loads RSS feed from URL", function(done) {
             this.timeout(10000);
             AudioManager.reset();
-            AudioManager.load('URL', 'http://bespoken.libsyn.com/rss', function () {
+            AudioManager.load('URL', 'http://bespoken.libsyn.com/rss', 60, function () {
                 assert.equal(AudioManager.feed().items.length, 3);
                 assert(AudioManager.feed());
                 AudioManager.introductorySSML(function (ssml) {
