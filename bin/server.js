c4159267c07313de9176922eb0e19fce3d4510bd
Pier Castaneda <pier.castaneda@gmail.com>
2019-01-09 11:49:39 -0500

    add running server log

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,6 @@
 const bst = require('bespoken-tools');
 
 const server = new bst.LambdaServer('lib/index.js', 10000, true);
-server.start();
+server.start(function() {
+    console.log("Server running on port 10000");
+});
